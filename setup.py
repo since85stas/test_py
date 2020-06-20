0cb9668be4092298fad62a539ceafebb87b22a56
Note <seeyou85@gmail.com>
2020-06-20 09:45:28 +0300

    Test python dir

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='test_py',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Admin',
+    author_email='',
+    description=''
+)
