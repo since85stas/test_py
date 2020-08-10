2250731a9098833dd1148c515c16c6e981bf871d
home <seeyou85@gmail.com>
2020-08-10 10:50:23 +0400

    training model

--- presure_test/keras_test.py
+++ presure_test/keras_test.py
@@ -11,11 +11,12 @@ def classification_model(num_inputs, num_1layer_neur):
     model = Sequential()
     model.add(Dense(num_inputs, activation='relu', input_shape=(num_inputs,)))
     model.add(Dense(num_1layer_neur, activation='relu'))
+    model.add(Dense(15, activation='relu'))
     # model.add(Dense(10, activation='relu'))
-    model.add(Dense(2, activation='softmax'))
+    model.add(Dense(1, activation='relu'))
 
     # compile model
-    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    model.compile(optimizer='adadelta', loss='squared_hinge', metrics=['accuracy'])
     return model
 
 def classify_keras_test_csv(features_train, labels_train):
@@ -37,18 +38,19 @@ def classify_keras(model ,features_train, labels_train, features_test, labels_te
     y_train = mass_to_nump_mass(labels_train)
     X_test = mass_to_nump_mass(features_test)
     y_test = mass_to_nump_mass(labels_test)
-    # model = classification_model(num_inp)
+    model = classification_model(num_inp,15)
 
-    y_binary = to_categorical(y_train)
-    y_binary_test = to_categorical(y_test)
+    # y_binary = to_categorical(y_train)
+    # y_binary_test = to_categorical(y_test)
 
     # fit the model
-    model.fit(X_train, y_binary, validation_data=(X_test, y_binary_test), epochs=10, verbose=2)
+    model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, verbose=2)
 
     # evaluate the model
-    scores = model.evaluate(X_test, y_binary_test, verbose=0)
+    scores = model.evaluate(X_test, y_test, verbose=0)
 
     print('Accuracy: {}% \n Error: {}'.format(scores[1], 1 - scores[1]))
+    return model
 
 def test_diff_model_shapes(features_train, labels_train, features_test, labels_test, num_inp):
     for i in range(2,15):
